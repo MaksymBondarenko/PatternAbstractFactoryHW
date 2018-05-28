7e967c732b4ed1017aa4c0519b9337545fea542c
MaksymBondarenko <you@example.com>
2018-05-28 16:26:24 +0300

    28.05.2018

---
+++ ConsoleApp1/ConsoleApp1/Factories/MercedesFactory.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp1.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Factories
+{
+    public class MercedesFactory : CarFactory
+    {
+        public override FamilyCar CreateFamilyCar()
+        {
+            return new MercedesFamilyCar();
+        }
+
+        public override SportCar CreateSportCar()
+        {
+            return new MercedesFamilyCar();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Products/MercedesFamilyCar.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Products
+{
+    public class MercedesFamilyCar : SportCar
+    {
+        public override void Speed()
+        {
+            Console.WriteLine("Speed=200 km");
+        }
+    }
+}
